b631313a735ef40d6377de4333a6df6d8addd269
Hari Prakash V <hariprakash2113@gmail.com>
2022-01-11 18:33:27 +0530

    Completed Admin UserPage details

--- Admin.java
+++ Admin.java
@@ -47,7 +47,43 @@ public class Admin {
         System.out.println("    -> Enter 7 to search a book");
         System.out.println("    -> Enter 8 to Edit Fine Limit");
         System.out.println("    -> Enter 9 to Generate reports");
-        System.out.println("    -> Enter 0 to Exit");
+        System.out.println("    -> Enter 0 to Logout");
         int n = Integer.parseInt(Main.sc.nextLine());
+        switch (n) {
+            case 1:
+                // Book.addBook();
+                break;
+            case 2:
+                // Book.modify();
+                break;
+            case 3:
+                // Book.deleteBook();
+                break;
+            case 4:
+                // Admin.addNewAdmin();
+                break;
+            case 5:
+                // User.addUser();
+                break;
+            case 6:
+                // Book.viewBooks();
+                break;
+            case 7:
+                // Book.search();
+                break;
+            case 8:
+                // User.fineLimit();
+                break;
+            case 9:
+                // Book.generateReport();
+                break;
+            case 0:
+                Main.homepage();
+                break;
+            default:
+                System.out.println("Invalid Input");
+                System.out.println("Enter Correct Choice");
+                adminPage(ind);
+        }
     }
 }
