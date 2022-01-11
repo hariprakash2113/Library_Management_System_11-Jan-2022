2aaa97fc8d607ed04238d619ead46e4f61535d61
Hari Prakash V <hariprakash2113@gmail.com>
2022-01-11 18:23:22 +0530

    Completed Login Credentials for Admin

---
+++ Admin.java
@@ -0,0 +1,53 @@
+public class Admin {
+
+    String name;
+    String password;
+    String email;
+
+    public Admin(String name, String password, String userId) {
+        this.name = name;
+        this.password = password;
+        this.email = userId;
+    }
+
+    static void login() {
+        System.out.println("-----Welcome Admin-----");
+        System.out.println("Enter your Email ID or 0 to Exit : ");
+        String email = Main.sc.nextLine();
+        int ind = -1;
+        for (int i = 0; i < Main.admins.size(); i++) {
+            if (Main.admins.get(i).email.equals(email)) {
+                ind = i;
+            }
+        }
+        if (ind == -1) {
+            System.out.println("Email ID not found");
+            login();
+        } else {
+            System.out.print("Enter Password : ");
+            String pass = Main.sc.nextLine();
+            if (Main.admins.get(ind).password.equals(pass)) {
+                adminPage(ind);
+            } else {
+                System.out.println("Wrong Password ! Try Again");
+                login();
+            }
+        }
+    }
+
+    static void adminPage(int ind) {
+        System.out.printf("----Welcome , %s -----", Main.admins.get(ind).name);
+        System.out.println("Please Select an option");
+        System.out.println("    -> Enter 1 for Adding a book");
+        System.out.println("    -> Enter 2 for Modifing Book Details");
+        System.out.println("    -> Enter 3 for Deleting a book");
+        System.out.println("    -> Enter 4 to Add a admin");
+        System.out.println("    -> Enter 5 to add a User");
+        System.out.println("    -> Enter 6 to view List of Books");
+        System.out.println("    -> Enter 7 to search a book");
+        System.out.println("    -> Enter 8 to Edit Fine Limit");
+        System.out.println("    -> Enter 9 to Generate reports");
+        System.out.println("    -> Enter 0 to Exit");
+        int n = Integer.parseInt(Main.sc.nextLine());
+    }
+}
---
+++ Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        homepage();
+    }
+
+    static ArrayList<Admin> admins = new ArrayList<>();
+
+    static {
+        admins.add(new Admin("Hari", "1108", "hari"));
+    }
+
+    static void homepage() {
+        System.out.println("-----LIBRARY MANAGEMENT SYSTEM-----");
+        System.out.println("    -> Enter 1 for Admin Login");
+        System.out.println("    -> Enter 2 for User Login");
+        System.out.println("    -> Enter 3 to exit");
+        System.out.print("Enter Choice : ");
+        int n = Integer.parseInt(sc.nextLine());
+        if (n == 1) {
+            // Admin.login();
+        } else if (n == 2) {
+            // User.login();
+        } else if (n == 3) {
+            System.exit(0);
+        } else {
+            System.out.println("Invalid Choice !\nEnter correct option");
+            homepage();
+        }
+    }
+}
